6384073437784b40904a2a239a53c5cb1438af2e
Tresor <tr.clarsi@gmail.com>
2025-04-21 10:40:50 +0100

    New Code

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class MyModel(models.Model): name = models.CharField(max_length=100)
+class MyModel(models.Model): name = models.CharField(max_length=150)
 # Create your models here.
 def __str__(self): return self.name
